986c5bbb95081100074e9b874e60f98a6a94be8a
pavani upadhyay <pavaniupadhyay@gmail.com>
2024-08-08 18:20:18 -0700

    inital commit

---
+++ project/app/page.tsx
@@ -0,0 +1,15 @@
+import { Main } from "next/document";
+import Image from "next/image";
+import Hero from "../components/Hero";
+
+
+export default function Home() {
+  return (
+    <main >
+      <div >
+        <Hero/>
+     
+      </div> 
+      </main>
+  );
+} 
